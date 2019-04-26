45138080ff14ebc4c1f17249a58cb073f802715a
Marek Bareła <marek.barela@wp.pl>
2019-04-26 11:51:57 +0200

    layout small change

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../header/Header';
 import Navigation from '../navigation/Navigation';
-import Grid from '@material-ui/core/Grid';
 import { Gender } from '../../features/utils/gender';
 import { NextFunctionComponent } from 'next';
 
@@ -14,13 +13,13 @@ interface Props {
 const Layout: NextFunctionComponent<Props> = props => {
   const { children, URL, nav } = props;
   return (
-    <Grid>
+    <>
       <Header URL={URL} />
       <Navigation nav={nav} />
       <main>
         {children}
       </main>
-    </Grid>
+    </>
   )
 }
 
